18e0ef8e5ca2955f5461c7aeb9a891d30b4443b5
venusofstar <sumidomikeangelo24@gmail.com>
2026-01-30 13:42:17 +0800

    Update episode.js

--- functions/episode.js
+++ functions/episode.js
@@ -1,21 +1,28 @@
-export async function onRequestGet({ request, env, params }) {
-  // Cloudflare Pages automatically passes URL params
+export async function onRequestGet({ request }) {
   const url = new URL(request.url);
-  const ep = parseInt(url.pathname.split("/").pop());
+  const parts = url.pathname.split("/");
+  const ep = parseInt(parts[parts.length - 1]);
 
-  let src = "";
-
-  if (ep >= 1 && ep <= 60) {
-    src = `https://dn720706.ca.archive.org/0/items/anime-2.0-5/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${ep}.mp4`;
-  } else if (ep <= 100) {
-    src = `https://ia601504.us.archive.org/10/items/anime-2.0-100/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${ep}.mp4`;
-  } else if (ep <= 148) {
-    src = `https://ia801509.us.archive.org/29/items/anime-2.0-114/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${ep}.mp4`;
-  } else {
+  if (!ep || ep < 1 || ep > 148) {
     return new Response(JSON.stringify({ error: "Episode not found" }), { status: 404 });
   }
 
+  let src = "";
+
+  if (ep <= 60) {
+    src = `https://dn720706.ca.archive.org/0/items/anime-2.0-5/HunterXHunter-${ep}.mp4`;
+  } else if (ep <= 100) {
+    src = `https://ia601504.us.archive.org/10/items/anime-2.0-100/HunterXHunter-${ep}.mp4`;
+  } else {
+    src = `https://ia801509.us.archive.org/29/items/anime-2.0-114/HunterXHunter-${ep}.mp4`;
+  }
+
+  // Return JSON with the video URL
   return new Response(JSON.stringify({ src }), {
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      // Optional: allow CORS
+      "Access-Control-Allow-Origin": "*"
+    },
   });
 }
